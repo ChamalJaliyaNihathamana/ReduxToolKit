689d5ac93017ebf9a1dd3e88bd0245033b438587
ChamalJaliyaNihathamana <chamal.n@softcodeit.com>
2022-02-08 13:45:36 +0530

    immutability with libs

--- src/index.js
+++ src/index.js
@@ -1,43 +1,25 @@
-// immutability
-const person = {
-  name: "John",
-  address: {
-    country: "United States",
-    city: "Washington",
-  },
-};
+import { produce } from "immer";
 
-// copy the content of person to empty object
-//  third arg is optional (object with updated properties)
-const updated = Object.assign({}, person, { name: "Bob", age: "22" });
-// debugger;
+let book = { title: "Harry Potter" };
 
-// solution to below problem
-const solupdated = {
-  ...person,
-  address: { ...person.address, city: "New York" },
-  name: "Bob",
-};
-// debugger;
-// method 2 (but both this two method perform a shallow copy)
-const supdated = { ...person, name: "Bob" };
-supdated.address.city = "New York"; // this violate the immutability
-// meaning now city in the person changed to New York. at this point and after
-// debugger;
+function publish(book) {
+  return produce(book, (draftBook) => {
+    draftBook.isPublished = true;
+  });
+}
 
-// immutability with arrays
-const numbers = [1, 2, 3];
-
-//Adding in place of 2
-const index = numbers.indexOf(2);
-// up to 2 excluding 2
-const added = [...numbers.slice(0, index), 4, ...numbers.slice(index)];
+let updated = publish(book);
 debugger;
 
-// Remove 2
-const removed = numbers.filter((n) => n !== 2);
-debugger;
+// Using Immutable
 
-// Updating (replace 2 with 20)
-const updatedArr = numbers.map((n) => (n === 2 ? 20 : n));
-debugger;
+// import { Map } from "immutable";
+
+// let book = Map({ title: "Harry Potter" });
+
+// function publish(book) {
+//   return book.set("isPublished", true);
+// }
+
+// publish(book);
+// debugger;

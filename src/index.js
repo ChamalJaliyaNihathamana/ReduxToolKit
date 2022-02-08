5add7dc7ac70af6e52e1adc666b1ccd5bcfe0e44
ChamalJaliyaNihathamana <chamal.n@softcodeit.com>
2022-02-08 13:28:27 +0530

    Immutability

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import { compose, pipe } from "lodash/fp";
+// immutability
+const person = {
+  name: "John",
+  address: {
+    country: "United States",
+    city: "Washington",
+  },
+};
 
-let input = "   Javascript  ";
-let output = "<div>" + input.trim() + "</div>";
+// copy the content of person to empty object
+//  third arg is optional (object with updated properties)
+const updated = Object.assign({}, person, { name: "Bob", age: "22" });
+// debugger;
 
-// Currying
-const trim = (str) => str.trim();
-const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
-const toLowerCase = (str) => str.toLowerCase();
+// solution to below problem
+const solupdated = {
+  ...person,
+  address: { ...person.address, city: "New York" },
+  name: "Bob",
+};
+// debugger;
+// method 2 (but both this two method perform a shallow copy)
+const supdated = { ...person, name: "Bob" };
+supdated.address.city = "New York"; // this violate the immutability
+// meaning now city in the person changed to New York. at this point and after
+// debugger;
 
-const transform = pipe(trim, toLowerCase, wrap("div"));
-console.log(transform(input));
+// immutability with arrays
+const numbers = [1, 2, 3];
 
-// compose works as same way like typical functional composing "R->L"
-// pipe works other way "L -> R" (first thing in left)
+//Adding in place of 2
+const index = numbers.indexOf(2);
+// up to 2 excluding 2
+const added = [...numbers.slice(0, index), 4, ...numbers.slice(index)];
+debugger;
 
+// Remove 2
+const removed = numbers.filter((n) => n !== 2);
+debugger;
 
+// Updating (replace 2 with 20)
+const updatedArr = numbers.map((n) => (n === 2 ? 20 : n));
+debugger;
